dfcc1af032babba6e2a2efc76c2e311e77c4c2bb
LTD Studio 404 <hosting@404.ge>
2015-03-13 17:54:43 +0400

    add jquery mobile

--- index.php
+++ index.php
@@ -1,7 +1,31 @@
-<?php
-$ip = $_SERVER["REMOTE_ADDR"];
-if($ip != "176.73.234.42"){
-	header("Location: https://www.facebook.com/404.ge");
-}
-?>
-heu
+<?php
+
+$ip = $_SERVER["REMOTE_ADDR"];
+
+if($ip != "176.73.234.42"){
+
+	header("Location: https://www.facebook.com/404.ge");
+
+}
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Studio 404</title>
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="dist/css/bootstrap.min.css" />
+	<!-- Optional theme -->
+	<link rel="stylesheet" href="dist/css/bootstrap-theme.min.css" />
+	<!-- Latest compiled and minified JavaScript -->
+	<script src="dist/js/bootstrap.min.js"></script>
+	<!--Jquery mobile START-->
+	<link rel="stylesheet" href="dist/jquery.mobile-1.4.5/jquery.mobile-1.4.5.min.css" />
+	<script src="dist/jquery.mobile-1.4.5/jquery-1.11.2.min.js"></script>
+	<script src="dist/jquery.mobile-1.4.5/jquery.mobile-1.4.5.min.js"></script>
+	<!--Jquery mobile END-->
+</head>
+<body>
+Hi
+</body>
+</html>
